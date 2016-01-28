1d1814fbc456fe15fda20b4c1473877dd8bb13a7
dethstrobe <dethstrobe@gmail.com>
2016-01-28 08:38:47 -0700

    priority table - started

--- src/components/PriorityTableComponent.js
+++ src/components/PriorityTableComponent.js
@@ -5,14 +5,53 @@ import React from 'react';
 require('styles//PriorityTable.sass');
 
 class PriorityTableComponent extends React.Component {
-  render() {
-    return (
-      <div className="prioritytable-component">
-        Please edit src/components///PriorityTableComponent.js to update this component!
-      </div>
-    );
-  }
+	render() {
+		return (
+			<PriorityTable className="prioritytable-component" />
+		);
+	}
 }
+
+const PriorityLabel = props => {
+	return (
+		<tr>
+			<th>Priority</th>
+			<th>Metatype</th>
+			<th>Attributes</th>
+			<th>Magic/Resonance</th>
+			<th>Skills</th>
+			<th>Resources</th>
+		</tr>
+	);
+};
+
+const PriorityDataCell = ({ rating }) => {
+	return (
+		<th>
+			{rating}
+		</th>
+		
+	);
+};
+
+const PriorityRow = props => {
+	return (
+		<tr>
+			<PriorityDataCell rating={props.rating}/>
+		</tr>
+	);
+};
+
+const PriorityTable = props => {
+	return (
+		<table>
+			<tbody>
+				<PriorityLabel />
+				<PriorityRow rating="A"/>
+			</tbody>
+		</table>
+	);
+};
 
 PriorityTableComponent.displayName = 'PriorityTableComponent';
 
