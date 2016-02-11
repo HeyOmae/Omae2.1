00bcfff47a6699332bfae16c86be8dec2ffd9a21
dethstrobe <dethstrobe@gmail.com>
2016-02-11 15:48:50 -0700

    Skills - rendering active skills

--- src/components/skills/ActiveSkillsComponent.js
+++ src/components/skills/ActiveSkillsComponent.js
@@ -13,27 +13,71 @@ class ActiveSkillsComponent extends React.Component {
 
 				<div className="row">
 					<div className="col-xs-12">
-						<div className="table-responsive">
-							<table className='table'>
-								<thead>
-									<tr>
-										<th>Raise</th>
-										<th>Rating</th>
-										<th>Lower</th>
-										<th>Skill Name</th>
-										<th>Spec</th>
-										<th>Mods</th>
-										<th>Dice Pool</th>
-										<th>Ref</th>
-									</tr>
-								</thead>
-							</table>
-						</div>
+						<ActiveSkill attribute={'agi'} skillList={skillsData.active['agi']}/>
 					</div>
 				</div>
 			</div>
 		);
 	}
+}
+
+const ActiveSkill = ({attribute, skillList}) => {
+	let skillTableData = [];
+	for(let skillName in skillList) {
+		let skillData = skillList[skillName],
+		specilizationOptions = skillData.specializations.map((spec) => {
+			return <option key={spec} value={spec}>spec</option>
+		});
+
+		skillTableData.push(
+			<tr key={skillData.name}>
+				<td>
+					<button className="btn btn-success">+</button>
+				</td>
+				<td>
+					0
+				</td>
+				<td>
+					<button className="btn btn-warning">-</button>
+				</td>
+				<td>
+					<strong>{skillData.name}</strong>
+				</td>
+				<td>
+					{skillData.reference.book} p{skillData.reference.page}
+				</td>
+				<td>
+					<select>
+						{specilizationOptions}
+					</select>
+				</td>
+				<td>0</td>
+				<td>0</td>
+			</tr>
+		);
+	}
+	return (
+		<div className="table-responsive">
+			<h4>{attribute}</h4>
+			<table className='table'>
+				<thead>
+					<tr>
+						<th>Raise</th>
+						<th>Rating</th>
+						<th>Lower</th>
+						<th>Skill Name</th>
+						<th>Ref</th>
+						<th>Spec</th>
+						<th>Mods</th>
+						<th>Dice Pool</th>
+					</tr>
+				</thead>
+				<tbody>
+					{skillTableData}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 ActiveSkillsComponent.displayName = 'SkillsActiveSkillsComponent';
