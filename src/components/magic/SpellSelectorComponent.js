aced96946fd23a7bf407c27137ed47347fd8b80d
dethstrobe <dethstrobe@gmail.com>
2016-04-30 23:47:33 -0600

    Spells - more refactoring to make code more reusable.

--- src/components/magic/SpellSelectorComponent.js
+++ src/components/magic/SpellSelectorComponent.js
@@ -6,6 +6,77 @@ const spellData = require('json!../data/spells.json');
 
 require('styles/magic/SpellSelector.sass');
 
+//helper functions
+function createSpellCategoryLabel (SpellsObj, {category, damage}) {
+	if(!SpellsObj[category]) {
+		SpellsObj[category] = [];
+		const spellLabel = category + '-label';
+		SpellsObj[category].push(
+			<tr key={spellLabel} className={spellLabel}>
+				<th>Learn</th>
+				<th>Spell</th>
+				<th>Descriptor</th>
+				<th>Type</th>
+				<th>Range</th>
+				{damage === '0' ? null: <th>Damage</th>}
+				<th>Duration</th>
+				<th>Drain</th>
+				<th>Ref</th>
+			</tr>
+		);
+	}
+
+	return SpellsObj;
+}
+
+function createSpellNameWithOptions(spellName) {
+	let partsOfName = {
+		start: '',
+		end: '',
+		placeholderText: ''
+	},
+	nameWithNoOptions = spellName.replace(/\[[\s\S]*\]/g, (match, offset, string) => {
+		partsOfName.placeholderText = match;
+		partsOfName.start = offset === 0 ? '' : string.slice(0, offset);
+		partsOfName.end = string.slice(string.indexOf(']') + 1);
+		return '';
+	});
+	if (!partsOfName.start && !partsOfName.end) {
+		partsOfName.start = nameWithNoOptions;
+	}
+
+	return partsOfName;
+}
+
+function createSpellTable(spellArray, spellName, spellDetails, button, spellID) {
+	spellArray[spellDetails.category].push(
+		<tr key={'spell-'+ (spellID)}>
+			{button}
+			<td>
+				{spellName.start}
+				{spellName.placeholderText ?
+					<input
+						className="form-control spell-option"
+						type="text"
+						ref={'spellOption'+spellDetails.name}
+						placeholder={spellName.placeholderText}/>
+					:null
+				}
+				{spellName.end||null}
+				</td>
+			<td>{spellDetails.descriptor}</td>
+			<td>{spellDetails.type}</td>
+			<td>{spellDetails.range}</td>
+			{spellDetails.damage === '0' ? null: <td>{spellDetails.damage}</td>}
+			<td>{spellDetails.duration}</td>
+			<td>{spellDetails.dv}</td>
+			<td>{spellDetails.source + ' p' + spellDetails.page}</td>
+		</tr>
+	);
+
+	return spellArray;
+}
+
 class SpellSelectorComponent extends React.Component {
 	render() {
 		const {addSpell, removeSpell, selectedSpells} = this.props;
@@ -17,86 +88,24 @@ class SpellSelectorComponent extends React.Component {
 
 		//generated spell details to populate addSpellModals
 		spellData.forEach((spell)=>{
-			function createSpellCategoryLabel (SpellsObj, {category, damage}) {
-				if(!SpellsObj[category]) {
-					SpellsObj[category] = [];
-					const spellLabel = category + '-label';
-					SpellsObj[category].push(
-						<tr key={spellLabel} className={spellLabel}>
-							<th>Learn</th>
-							<th>Spell</th>
-							<th>Descriptor</th>
-							<th>Type</th>
-							<th>Range</th>
-							{damage === '0' ? null: <th>Damage</th>}
-							<th>Duration</th>
-							<th>Drain</th>
-							<th>Ref</th>
-						</tr>
-					);
-				}
-
-				return SpellsObj;
-			}
-
 			spells = createSpellCategoryLabel(spells, spell);
 
-			function createSpellNameWithOptions(spellName) {
-				let partsOfName = {
-					start: '',
-					end: '',
-					placeholderText: ''
-				},
-				nameWithNoOptions = spellName.replace(/\[[\s\S]*\]/g, (match, offset, string) => {
-					partsOfName.placeholderText = match;
-					partsOfName.start = offset === 0 ? '' : string.slice(0, offset);
-					partsOfName.end = string.slice(string.indexOf(']') + 1);
-					return '';
-				});
-				if (!partsOfName.start) {
-					partsOfName.start = nameWithNoOptions;
-				}
-
-				return partsOfName;
-			}
-
 			let spellName = createSpellNameWithOptions(spell.name),
-				spellNameOptions = this.refs['spellOption'+spell.name] ? this.refs['spellOption'+spell.name].value : '',
+				//put this click function into a closure function tomorrow.
 				addSpellClick = () => {
-					let newName = spellName.start + spellNameOptions + spellName.end,
+					let spellNameOptions = this.refs['spellOption'+spell.name] ? this.refs['spellOption'+spell.name].value : '',
+						newName = spellName.start + spellNameOptions + spellName.end,
 						spellToAdd = Object.assign(
 							{},
 							spell,
 							{name: newName}
 						);
 					addSpell({newSpell: spellToAdd});
-				};
+				},
+				addSpellButton = <td><button className="btn btn-success" onClick={addSpellClick}>+</button></td>;
 
 			//need to make a Extended class component to make this work
-			spells[spell.category].push(
-				<tr key={'spell-'+ (spellID++)}>
-					<td><button className="btn btn-success" onClick={addSpellClick}>+</button></td>
-					<td>
-						{spellName.start}
-						{spellName.placeholderText ?
-							<input
-								className="form-control spell-option"
-								type="text"
-								ref={'spellOption'+spell.name}
-								placeholder={spellName.placeholderText}/>
-							:null
-						}
-						{spellName.end||null}
-						</td>
-					<td>{spell.descriptor}</td>
-					<td>{spell.type}</td>
-					<td>{spell.range}</td>
-					{spell.damage === '0' ? null: <td>{spell.damage}</td>}
-					<td>{spell.duration}</td>
-					<td>{spell.dv}</td>
-					<td>{spell.source + ' p' + spell.page}</td>
-				</tr>
-			);
+			spells = createSpellTable(spells, spellName, spell, addSpellButton, spellID++);
 		});
 
 		for(let spellCat in spells) {
