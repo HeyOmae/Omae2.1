745db17cca0ac0c31c43fb5f312703a891e23459
dethstrobe <dethstrobe@gmail.com>
2017-05-04 21:24:48 -0600

    Armormods - rendering all the mods

--- src/components/gear/ArmorModsComponent.js
+++ src/components/gear/ArmorModsComponent.js
@@ -2,24 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import armorMods from '../../data/armorAccessories.json';
 
-function ArmorModsComponent() {
+function ArmorModsComponent({armorName}) {
 	return (
 		<div className="col">
 			<p><strong>Capacity:</strong> {0}</p>
 			<table className="table table-striped">
 				<tbody>
 					<tr>
-						<td>
-							<input
-								type="checkbox"
-								className="form-control"
-							/>
-						</td>
+						{
+						armorMods.map((mod) => {
+							return (
+								<td key={`${armorName}-mod-${mod.name}`}>
+									<input
+										id={`${armorName}-mod-${mod.name}`}
+										type="checkbox"
+										className="form-control"
+									/>
+									<label
+										htmlFor={`${armorName}-mod-${mod.name}`}
+									>
+										{mod.name}
+									</label>
+								</td>
+							);
+						})
+						}
 					</tr>
 				</tbody>
 			</table>
 		</div>
 	);
 }
+
+ArmorModsComponent.propTypes = {
+	armorName: PropTypes.string.isRequired
+};
 
 export default ArmorModsComponent;
