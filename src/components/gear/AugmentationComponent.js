11dfe0443f02db80aac2e76416c6271b17d11846
dethstrobe <dethstrobe@gmail.com>
2017-06-20 21:32:46 -0600

    Augs - I don't know...maybe put eyes and ears over here...who knows...

--- src/components/gear/AugmentationComponent.js
+++ src/components/gear/AugmentationComponent.js
@@ -19,12 +19,24 @@ class AugmentationComponent extends React.PureComponent {
 	render() {
 		return (
 			<div className="augs row">
-				<div className="augs--head col-md-12">
+				<div className="augs--eyes-ears col-md-4">
 					<div className="btn-group-vertical">
-						<label htmlFor="augs-head-partial" className="btn btn-primary active">
+						<button className="btn btn-info">
+							Eyes
+						</button>
+						<button className="btn btn-info">
+							Ears
+						</button>
+					</div>
+				</div>
+
+
+				<div className="augs--head col-md-2">
+					<div className="btn-group-vertical">
+						<label htmlFor="augs-head-partial" className="btn btn-primary btn-block active">
 							<input type="radio" name="Partial Cyberskull" id="augs-head-partial" checked /> Partial
 						</label>
-						<label htmlFor="augs-head-full" className="btn btn-primary active">
+						<label htmlFor="augs-head-full" className="btn btn-primary btn-block">
 							<input type="radio" name="Obvious Skull" id="augs-head-full" checked /> Full
 						</label>
 					</div>
