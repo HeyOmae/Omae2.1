473233c13d2596371ab96876f2befef0a8ebb06d
dethstrobe <dethstrobe@gmail.com>
2017-06-26 22:35:13 -0600

    augs - body or something

--- src/components/gear/AugmentationComponent.js
+++ src/components/gear/AugmentationComponent.js
@@ -19,79 +19,79 @@ class AugmentationComponent extends React.PureComponent {
 	render() {
 		return (
 			<div className="augs">
-			<div className="augs--head row">
-				<div className="augs--hear__eyes-ears col-md-5">
-					<div className="btn-group-vertical">
-						<button className="btn btn-info">
-							Eyes
-						</button>
-						<button className="btn btn-info">
-							Ears
-						</button>
+				<div className="augs--head row">
+					<div className="augs--hear__eyes-ears col-md-5">
+						<div className="btn-group-vertical">
+							<button className="btn btn-info">
+								Eyes
+							</button>
+							<button className="btn btn-info">
+								Ears
+							</button>
+						</div>
+					</div>
+
+
+					<div className="augs--head__skull col-md-2">
+						<div className="btn-group-vertical">
+							<label htmlFor="augs-head-partial" className="btn btn-primary btn-block active">
+								<input type="radio" name="Partial Cyberskull" id="augs-head-partial" checked /> Partial
+							</label>
+							<label htmlFor="augs-head-full" className="btn btn-primary btn-block">
+								<input type="radio" name="Obvious Skull" id="augs-head-full" checked /> Full
+							</label>
+						</div>
 					</div>
 				</div>
 
 
-				<div className="augs--head__skull col-md-2">
-					<div className="btn-group-vertical">
-						<label htmlFor="augs-head-partial" className="btn btn-primary btn-block active">
-							<input type="radio" name="Partial Cyberskull" id="augs-head-partial" checked /> Partial
-						</label>
-						<label htmlFor="augs-head-full" className="btn btn-primary btn-block">
-							<input type="radio" name="Obvious Skull" id="augs-head-full" checked /> Full
-						</label>
+				<div className="augs--body row">
+					<div className="augs--right__right-arm col-md-4">
+						<div className="btn-group">
+							<label htmlFor="augs-right-none" className="btn btn-primary btn-block active">
+								<input type="radio" name="Hand" id="augs-right-none" checked /> None
+							</label>
+							<label htmlFor="augs-right-hand" className="btn btn-primary btn-block active">
+								<input type="radio" name="Hand" id="augs-right-hand" checked /> Hand
+							</label>
+							<label htmlFor="augs-right-lower" className="btn btn-primary btn-block">
+								<input type="radio" name="Lower" id="augs-right-lower" checked /> Lower
+							</label>
+							<label htmlFor="augs-right-arm" className="btn btn-primary btn-block">
+								<input type="radio" name="Arm" id="augs-right-arm" checked /> Arm
+							</label>
+						</div>
+					</div>
+
+					<div className="augs--body__torso col-md-2">
+						<div className="btn-group-vertical">
+							<label htmlFor="augs-body-no-torso" className="btn btn-primary btn-block ">
+								<input type="radio" name="" id="augs-body-no-torso" checked /> None
+							</label>
+							<label htmlFor="augs-body-full" className="btn btn-primary btn-block">
+								<input type="radio" name="Torso" id="augs-body-torso" checked /> Torso
+							</label>
+						</div>
+					</div>
+
+					<div className="augs--left__left-arm col-md-4">
+						<div className="btn-group">
+							<label htmlFor="augs-left-arm" className="btn btn-primary btn-block">
+								<input type="radio" name="Arm" id="augs-left-arm" checked /> Arm
+							</label>
+							<label htmlFor="augs-left-lower" className="btn btn-primary btn-block">
+								<input type="radio" name="Lower" id="augs-left-lower" checked /> Lower
+							</label>
+							<label htmlFor="augs-left-hand" className="btn btn-primary btn-block active">
+								<input type="radio" name="Hand" id="augs-left-hand" checked /> Hand
+							</label>
+							<label htmlFor="augs-left-none" className="btn btn-primary btn-block active">
+								<input type="radio" name="Hand" id="augs-left-none" checked /> None
+							</label>
+						</div>
 					</div>
 				</div>
 			</div>
-
-
-			<div className="augs--body row">
-				<div className="augs--right__right-arm col-md-4">
-					<div className="btn-group">
-						<label htmlFor="augs-right-none" className="btn btn-primary btn-block active">
-							<input type="radio" name="Hand" id="augs-right-none" checked /> None
-						</label>
-						<label htmlFor="augs-right-hand" className="btn btn-primary btn-block active">
-							<input type="radio" name="Hand" id="augs-right-hand" checked /> Hand
-						</label>
-						<label htmlFor="augs-right-lower" className="btn btn-primary btn-block">
-							<input type="radio" name="Lower" id="augs-right-lower" checked /> Lower
-						</label>
-						<label htmlFor="augs-right-arm" className="btn btn-primary btn-block">
-							<input type="radio" name="Arm" id="augs-right-arm" checked /> Arm
-						</label>
-					</div>
-				</div>
-
-				<div className="augs--body__torso col-md-2">
-					<div className="btn-group-vertical">
-						<label htmlFor="augs-body-no-torso" className="btn btn-primary btn-block ">
-							<input type="radio" name="" id="augs-body-no-torso" checked /> None
-						</label>
-						<label htmlFor="augs-body-full" className="btn btn-primary btn-block">
-							<input type="radio" name="Torso" id="augs-body-torso" checked /> Torso
-						</label>
-					</div>
-				</div>
-
-				<div className="augs--left__left-arm col-md-4">
-					<div className="btn-group">
-						<label htmlFor="augs-left-arm" className="btn btn-primary btn-block">
-							<input type="radio" name="Arm" id="augs-left-arm" checked /> Arm
-						</label>
-						<label htmlFor="augs-left-lower" className="btn btn-primary btn-block">
-							<input type="radio" name="Lower" id="augs-left-lower" checked /> Lower
-						</label>
-						<label htmlFor="augs-left-hand" className="btn btn-primary btn-block active">
-							<input type="radio" name="Hand" id="augs-left-hand" checked /> Hand
-						</label>
-						<label htmlFor="augs-left-none" className="btn btn-primary btn-block active">
-							<input type="radio" name="Hand" id="augs-left-none" checked /> None
-						</label>
-					</div>
-				</div>
-			</div>
-		</div>
 		);
 	}
 }
