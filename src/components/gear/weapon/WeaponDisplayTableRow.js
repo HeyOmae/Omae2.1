832addd93dd5a567860088b2479670ae14361323
dethstrobe <dethstrobe@gmail.com>
2017-09-09 22:08:23 -0600

    Weapons - fixed import links, made weapon table row its own component

---
+++ src/components/gear/weapon/WeaponDisplayTableRow.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class WeaponsTableRow extends React.Component {
+	render() {
+		const {weapon, button, mod} = this.props;
+		return (
+			<tr key={weapon.name}>
+				<td>{button}</td>
+				{mod ? <td>{mod}</td> : <td>{weapon.name}</td>}
+				<td>{weapon.accuracy}</td>
+				<td>{weapon.damage}</td>
+				<td>{weapon.ap}</td>
+				<td>{weapon.mode === '0' ? 'N/A' : weapon.mode}</td>
+				<td>{weapon.type === 'Melee' ? weapon.reach : weapon.rc}</td>
+				<td>{weapon.avail}</td>
+				<td>{weapon.currentCost || weapon.cost}&yen;</td>
+				<td>{weapon.source} p{weapon.page}</td>
+			</tr>
+		);
+	}
+}
+
+WeaponsTableRow.propTypes = {
+	weapon: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		accuracy: PropTypes.string.isRequired,
+		damage: PropTypes.string.isRequired,
+		ap: PropTypes.string,
+		mode: PropTypes.string,
+		type: PropTypes.string.isRequired,
+		reach: PropTypes.string,
+		rc: PropTypes.string,
+		avail: PropTypes.string.isRequired,
+		cost: PropTypes.string.isRequired,
+		source: PropTypes.string.isRequired,
+		page: PropTypes.string.isRequired
+	}).isRequired,
+	button: PropTypes.element.isRequired,
+	mod: PropTypes.element
+};
+WeaponsTableRow.defaultProps = {
+	mod: null
+};
+
+export default WeaponsTableRow;
--- src/components/gear/weapon/WeaponsComponent.js
+++ src/components/gear/weapon/WeaponsComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import weaponData from 'data/weapons.json';
+import weaponMods from 'data/weaponAccessories.json';
+import 'styles/gear/Weapons.scss';
 import WeaponModsComponent from './WeaponModsComponent';
-import Modal from '../ModalButtonComponent';
-import FilterTable from '../FilterableTable';
-import DisplayTable from '../DisplayTableComponent';
-import weaponData from '../../data/weapons.json';
-import weaponMods from '../../data/weaponAccessories.json';
-import PropTypeChecking from '../../config/propTypeChecking';
+import Modal from '../../ModalButtonComponent';
+import FilterTable from '../../FilterableTable';
+import DisplayTable from '../../DisplayTableComponent';
+import PropTypeChecking from '../../../config/propTypeChecking';
+import WeaponsTableRow from './WeaponDisplayTableRow';
 
-import '../../styles/gear/Weapons.scss';
 
 class WeaponsComponent extends React.Component {
 	componentWillMount() {
@@ -179,45 +180,6 @@ WeaponTableHeader.propTypes = {
 WeaponTableHeader.defaultProps = {
 	buySell: 'Buy',
 	isModable: false
-};
-
-function WeaponsTableRow({weapon, button, mod}) {
-	return (
-		<tr key={weapon.name}>
-			<td>{button}</td>
-			{mod ? <td>{mod}</td> : <td>{weapon.name}</td>}
-			<td>{weapon.accuracy}</td>
-			<td>{weapon.damage}</td>
-			<td>{weapon.ap}</td>
-			<td>{weapon.mode === '0' ? 'N/A' : weapon.mode}</td>
-			<td>{weapon.type === 'Melee' ? weapon.reach : weapon.rc}</td>
-			<td>{weapon.avail}</td>
-			<td>{weapon.currentCost || weapon.cost}&yen;</td>
-			<td>{weapon.source} p{weapon.page}</td>
-		</tr>
-	);
-}
-
-WeaponsTableRow.propTypes = {
-	weapon: PropTypes.shape({
-		name: PropTypes.string.isRequired,
-		accuracy: PropTypes.string.isRequired,
-		damage: PropTypes.string.isRequired,
-		ap: PropTypes.string,
-		mode: PropTypes.string,
-		type: PropTypes.string.isRequired,
-		reach: PropTypes.string,
-		rc: PropTypes.string,
-		avail: PropTypes.string.isRequired,
-		cost: PropTypes.string.isRequired,
-		source: PropTypes.string.isRequired,
-		page: PropTypes.string.isRequired
-	}).isRequired,
-	button: PropTypes.element.isRequired,
-	mod: PropTypes.element
-};
-WeaponsTableRow.defaultProps = {
-	mod: null
 };
 
 WeaponsComponent.displayName = 'GearWeaponsComponent';
