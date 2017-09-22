7426ea1ffc92e0f39a6ff210485640e7d0f8a78c
dethstrobe <dethstrobe@gmail.com>
2017-09-21 21:27:51 -0600

    GearTableRow - starting to work on tests

--- test/components/gear/GearTableDisplayRowTest.js
+++ test/components/gear/GearTableDisplayRowTest.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import GearTableDislayRow from 'components/gear/GearTableDislayRow';
+import GearTableDislayRow from 'components/gear/GearTableDisplayRow';
 
 describe('<GearTableDisplayRow />', () => {
 	const setup = () => {
-		const props = {},
+		const props = {
+			gear: {
+				name: 'Banana',
+				avail: '3R',
+				source: 'BK',
+				page: '1337'
+			}
+		},
 			gearTableDislayRow = shallow(<GearTableDislayRow {...props} />);
 
 		return { gearTableDislayRow, props };
@@ -14,6 +21,8 @@ describe('<GearTableDisplayRow />', () => {
 	it('should display gear stats', () => {
 		const { gearTableDislayRow, props } = setup();
 
-		
+		expect(gearTableDislayRow.find('.gear-name').text()).to.equal('Banana');
+		expect(gearTableDislayRow.find('.gear-avail').text()).to.equal('3R');
+		expect(gearTableDislayRow.find('.gear-ref').text()).to.equal('BK p1337');
 	});
 });
