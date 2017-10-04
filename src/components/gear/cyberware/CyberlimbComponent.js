9347753be714f063c8d76721cf74abc921be84b9
dethstrobe <dethstrobe@gmail.com>
2017-10-03 20:43:15 -0600

    WareGradeComponent - refactored in to its own component with useless optimizations

--- src/components/gear/cyberware/CyberlimbComponent.js
+++ src/components/gear/cyberware/CyberlimbComponent.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 import metatypeData from 'data/metatype.json';
 import DisplayTable from '../../DisplayTableComponent';
 import waregrades from '../../../data/waregrade.json';
-import { purchaseGear, selectGrade } from '../../../actions';
+import { purchaseGear } from '../../../actions';
+import WareGradeComponent from './WareGradeComponent';
 
 class CyberlimbComponent extends React.PureComponent {
 	constructor(props) {
@@ -65,7 +66,7 @@ class CyberlimbComponent extends React.PureComponent {
 	}
 
 	render() {
-		const {cyberlimbsByType, location, changeGrade, currentGrade} = this.props;
+		const {cyberlimbsByType, location} = this.props;
 		return (
 			<div>
 				<h4>Cyber {location}</h4>
@@ -123,10 +124,7 @@ class CyberlimbComponent extends React.PureComponent {
 						</div>
 					</div>
 					<div className="col-12 col-md-4">
-						<WareGradeComponent
-							changeGrade={changeGrade}
-							currentGrade={currentGrade}
-						/>
+						<WareGradeComponent />
 					</div>
 				</div>
 				<div>
@@ -154,7 +152,6 @@ CyberlimbComponent.propTypes = {
 		).isRequired
 	).isRequired,
 	purchase: PropTypes.func.isRequired,
-	changeGrade: PropTypes.func.isRequired,
 	currentGrade: PropTypes.number.isRequired,
 	metatype: PropTypes.string.isRequired
 };
@@ -253,43 +250,6 @@ CyberlimbRadioSelect.propTypes = {
 	changeActiveType: PropTypes.func.isRequired
 };
 
-const WareGradeComponent = ({changeGrade, currentGrade}) => {
-	return (
-		<div className="form-group">
-			<label
-				htmlFor="ware-grade"
-			>
-				Grade
-			</label>
-			<select
-				id="ware-grade"
-				className="form-control custom-select"
-				onChange={(e) => {
-					changeGrade({grade: Number(e.target.value)});
-				}}
-				value={currentGrade}
-			>
-				{waregrades.map((grade, index) => {
-					return (
-						<option
-							value={index}
-							key={`cyber-${grade.name}`}
-						>
-							{grade.name}
-						</option>
-					);
-				})
-				}
-			</select>
-		</div>
-	);
-};
-
-WareGradeComponent.propTypes = {
-	changeGrade: PropTypes.func.isRequired,
-	currentGrade: PropTypes.number.isRequired
-};
-
 const mapStateToProps = (state) => {
 	return {
 		currentGrade: state.augmentation.grade,
@@ -299,8 +259,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	const actions = {
-		purchase: purchaseGear,
-		changeGrade: selectGrade
+		purchase: purchaseGear
 	};
 	return bindActionCreators(actions, dispatch);
 };
---
+++ src/components/gear/cyberware/WareGradeComponent.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import waregrades from '../../../data/waregrade.json';
+import { selectGrade } from '../../../actions';
+
+class WareGradeComponent extends React.Component {
+	constructor(props) {
+		super(props);
+		this.onGradeChange = this.onGradeChange.bind(this);
+	}
+	componentWillMount() {
+		this.gradeOptions = waregrades.map((grade, index) => {
+			return (
+				<option
+					value={index}
+					key={`cyber-${grade.name}`}
+					>
+					{grade.name}
+				</option>
+			);
+		});
+	}
+
+	onGradeChange(e) {
+		this.props.changeGrade({grade: Number(e.target.value)});
+	}
+
+	render() {
+		const {currentGrade} = this.props;
+		return (
+			<div className="form-group">
+				<label
+					htmlFor="ware-grade"
+				>
+					Grade
+				</label>
+				<select
+					id="ware-grade"
+					className="form-control custom-select"
+					onChange={this.onGradeChange}
+					value={currentGrade}
+				>
+					{this.gradeOptions}
+				</select>
+			</div>
+		);
+	}
+}
+
+WareGradeComponent.propTypes = {
+	changeGrade: PropTypes.func.isRequired,
+	currentGrade: PropTypes.number.isRequired
+};
+
+const mapStateToProps = (state) => {
+	return {
+		currentGrade: state.augmentation.grade,
+	};
+};
+
+export default connect(mapStateToProps, {changeGrade: selectGrade})(WareGradeComponent);
