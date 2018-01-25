b53f2304e80024805c1801c5c09981dec6cd69f0
dethstrobe <dethstrobe@gmail.com>
2018-01-24 20:59:25 -0700

    Mechs - prototype checking, i hope

--- src/components/gear/mech/MechComponent.js
+++ src/components/gear/mech/MechComponent.js
@@ -1,2 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+class MechComponent extends React.Component {
+	render() {
+		return ();
+	}
+}
+
+MechComponent.propTypes = {
+	mechType: PropTypes.string.isRequired,
+	mechs: PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		page: PropTypes.string.isRequired,
+		source: PropTypes.string.isRequired,
+		accel: PropTypes.string.isRequired,
+		armor: PropTypes.string.isRequired,
+		avail: PropTypes.string.isRequired,
+		body: PropTypes.string.isRequired,
+		category: PropTypes.string.isRequired,
+		cost: PropTypes.string.isRequired,
+		handling: PropTypes.string.isRequired,
+		pilot: PropTypes.string.isRequired,
+		sensor: PropTypes.string.isRequired,
+		speed: PropTypes.string.isRequired,
+	})).isRequired
+};
+
+export default MechComponent;
