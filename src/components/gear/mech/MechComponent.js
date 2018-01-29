e451c8a3aa0dcdc2d13e98aa2b7ffd66a86775e3
dethstrobe <dethstrobe@gmail.com>
2018-01-28 21:37:12 -0700

    MechComponent - starting to write tests

--- src/components/gear/mech/MechComponent.js
+++ src/components/gear/mech/MechComponent.js
@@ -3,27 +3,32 @@ import PropTypes from 'prop-types';
 
 class MechComponent extends React.Component {
 	render() {
-		return ();
+		const {classOfMechs, mechsByType} = this.props;
+		return (
+			<div>{classOfMechs} { mechsByType }</div>
+		);
 	}
 }
 
 MechComponent.propTypes = {
-	mechType: PropTypes.string.isRequired,
-	mechs: PropTypes.arrayOf(PropTypes.shape({
-		name: PropTypes.string.isRequired,
-		page: PropTypes.string.isRequired,
-		source: PropTypes.string.isRequired,
-		accel: PropTypes.string.isRequired,
-		armor: PropTypes.string.isRequired,
-		avail: PropTypes.string.isRequired,
-		body: PropTypes.string.isRequired,
-		category: PropTypes.string.isRequired,
-		cost: PropTypes.string.isRequired,
-		handling: PropTypes.string.isRequired,
-		pilot: PropTypes.string.isRequired,
-		sensor: PropTypes.string.isRequired,
-		speed: PropTypes.string.isRequired,
-	})).isRequired
+	classOfMechs: PropTypes.oneOf(['Vehicles', 'Drones']).isRequired,
+	mechsByType: PropTypes.objectOf(
+		PropTypes.arrayOf(PropTypes.shape({
+			name: PropTypes.string.isRequired,
+			page: PropTypes.string.isRequired,
+			source: PropTypes.string.isRequired,
+			accel: PropTypes.string.isRequired,
+			armor: PropTypes.string.isRequired,
+			avail: PropTypes.string.isRequired,
+			body: PropTypes.string.isRequired,
+			category: PropTypes.string.isRequired,
+			cost: PropTypes.string.isRequired,
+			handling: PropTypes.string.isRequired,
+			pilot: PropTypes.string.isRequired,
+			sensor: PropTypes.string.isRequired,
+			speed: PropTypes.string.isRequired,
+		})).isRequired
+	).isRequired
 };
 
 export default MechComponent;
--- test/components/gear/mech/MechComponentTest.js
+++ test/components/gear/mech/MechComponentTest.js
@@ -3,3 +3,25 @@ import { shallow } from 'enzyme';
 
 import MechComponent from 'components/gear/mech/MechComponent';
 
+describe('Mech Component', () => {
+	const setup = () => {
+		const props = {
+			classOfMechs: 'Vehicles',
+			mechsByType: {
+				bikes: [],
+				cars: [],
+				boats: []
+			}
+			
+		},
+			mechComponent = shallow(<MechComponent {...props} />);
+
+		return { props, mechComponent };
+	};
+
+	it('should render the class of the mech as a render', () => {
+		const {mechComponent} = setup();
+
+		expect(mechComponent.find('h3').text()).to.equal('Vehicles');
+	});
+});
