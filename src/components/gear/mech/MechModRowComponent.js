02d68f2a5a97292cd1bfe15d8146aea6e49ea865
dethstrobe <dethstrobe@gmail.com>
2018-03-08 20:20:08 -0700

    Mech - display mod stats in mod row

--- src/components/gear/mech/MechModRowComponent.js
+++ src/components/gear/mech/MechModRowComponent.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MechModRowComponent extends React.PureComponent {
 	render() {
+		const { mod } = this.props;
 		return (
 			<tr>
-				<td>testing</td>
+				<td>{mod.name}</td>
+				<td>{mod.rating}</td>
+				<td>{mod.slot || 'N/A'}</td>
+				<td>{mod.avail}</td>
+				<td>{mod.cost}&yen;</td>
+				<td>{mod.source} {mod.page}p</td>
 			</tr>
 		);
 	}
 }
+
+MechModRowComponent.propTypes = {
+	mod: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		rating: PropTypes.string.isRequired,
+		slot: PropTypes.string,
+		avail: PropTypes.string.isRequired,
+		cost: PropTypes.string.isRequired,
+		source: PropTypes.string.isRequired,
+		page: PropTypes.string.isRequired
+	}).isRequired
+};
 
 export default MechModRowComponent;
