ee4d5b4c0b171b17aadc1a06cd62da08e92976b4
dethstrobe <dethstrobe@gmail.com>
2018-10-30 19:00:11 -0600

    LifeStyle Component - starting to work on it

---
+++ src/components/lifestyles/LifeStyleComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LifeStyleComponent extends React.PureComponent {
+	render() {
+		return (
+			<div className="life-style-component">
+				<h3>Life Styles</h3>
+			</div>
+		);
+	}
+}
+
+export default LifeStyleComponent;
