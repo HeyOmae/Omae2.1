55f54c4a94b971cb32af5cf1b3949e3bd0cafc85
dethstrobe <dethstrobe@gmail.com>
2019-03-12 18:40:52 -0700

    Lifestyle -added test for lifestyle component

---
+++ test/components/lifestyles/LifeStyleComponentTest.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import LifeStyleComponent from 'components/lifestyles/LifeStyleComponent';
+
+describe('<LifeStyleComponent />', () => {
+	const setup = () => {
+		const props = {},
+			lifeStyle = shallow(<LifeStyleComponent {...props} />);
+
+		return { lifeStyle, props };
+	};
+
+	it('should exist', () => {
+		const {lifeStyle} = setup();
+
+		expect(lifeStyle).to.be.exist;
+	});
+});
